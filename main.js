2b30f9ebd4999c2c605f6d13c5108770fd0109af
mattomolewa <matthew.omolewa@gmail.com>
2020-04-09 21:55:06 +0100

    updated main.js file

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-console.log("hello world");
-let result = "matthew";
-console.log(`${result} is mine`);
+const password = "pass";
+
+if (password.length >= 12 && password.includes("@")) {
+  console.log("well done ninja!");
+} else if (password.lenght >= 8) {
+  console.log("not bad!!");
+} else {
+  console.log("man up sissy!");
+}
